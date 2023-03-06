e88ba33d95cd424765ca9bf2b551e4f77b48aee5
Ordanis Sanchez <ordanisanchez@gmail.com>
2023-03-06 17:16:16 -0400

    Format navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,19 +1,25 @@
-import Link from "next/link"
-import Image from "next/image"
+import Link from "next/link";
+import Image from "next/image";
 
-import Berchain from "../public/berachain.svg"
+import Berchain from "../public/berachain.svg";
 
 export default function Navbar() {
-    return (
-      <header className="navbar">
-        <div className="flex-1">
-          <Image src={Berchain} alt="Berachain logo" />
-        </div>
-        <div className="flex-none">
-          <Link className="mr-7" href="/">Home</Link>
-          <Link className="mr-7" href="/transactions">Transactions</Link>
-          <Link className="mr-10" href="/blocks">Blocks</Link>
-        </div>
-      </header>
-    )
+  return (
+    <header className="navbar">
+      <div className="flex-1">
+        <Image src={Berchain} alt="Berachain logo" />
+      </div>
+      <div className="flex-none">
+        <Link className="mr-7 text-dark-brown" href="/">
+          Home
+        </Link>
+        <Link className="mr-7 text-dark-brown" href="/transactions">
+          Transactions
+        </Link>
+        <Link className="mr-10 text-dark-brown" href="/blocks">
+          Blocks
+        </Link>
+      </div>
+    </header>
+  );
 }
