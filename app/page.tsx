b327d436c0c98bfe165c022c1936ab7c97695680
Ordanis Sanchez <ordanisanchez@gmail.com>
2023-04-03 14:28:18 -0400

    Fix ts

--- app/page.tsx
+++ app/page.tsx
@@ -28,6 +28,7 @@ export default async function Home() {
       </h1>
       <SearchBar />
       <div className="grid grid-cols-1 md:grid-cols-2 gap-7 mt-14 mb-9 w-11/12 self-center">
+        {/* @ts-expect-error Async Server Component */}
         <Stats />
         <div className="flex flex-col bg-white px-4 py-6 rounded-xl shadow-xl overflow-x-auto">
           <h6 className="ml-3 mb-2 text-xl text-gray-3">Latest Blocks</h6>
