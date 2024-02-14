ead07c92a2df51e4951f988fce59361f409cabcf
XJuanCarlosXD <juancarlos192003@gmail.com>
2024-02-14 15:12:17 -0400

    update 5 files

--- app/address/[hash]/page.tsx
+++ app/address/[hash]/page.tsx
@@ -7,6 +7,7 @@ import SearchBar from "@/components/SearchBar";
 import AddressTransactions from "@/components/AddressTransactions";
 import { getTransactionsByAddress } from "@/utils/txns";
 import { getAccount } from "@/utils/accounts";
+import TransactionsChart from "@/components/TransactionsChart";
 
 export default async function Address({
   params: { hash: address },
@@ -28,68 +29,122 @@ export default async function Address({
     block_height: txn.block_height.toString(),
     gas: txn.gas.toString(),
   }));
+  const weeksArray = Array.from({ length: 10 }, (_, i) => ({
+    date: `week ${(i + 1).toString().padStart(2, "0")}`,
+    count: i === 5 ? 300 : 100 * i,
+  }));
 
   return (
-    <div className="grow mx-4 md:mx-6 lg:mx-24 mb-14">
-      <div className="flex flex-col items-start my-5 lg:flex-row lg:items-center lg:justify-between lg:my-10">
-        <h1 className="mb-3 lg:mb-0 text-gray-3 text-2xl">
-          {account?.is_contract ? "Contract" : "Account"}
-        </h1>
-        <SearchBar />
-      </div>
-      <div className="grid grid-cols-1 md:grid-cols-5 gap-y-4 md:gap-4">
-        <div className="col-span-3 px-8 py-10 space-y-7 bg-white rounded-xl shadow-xl text-gray-4 text-sm">
-          <h6 className="font-semibold text-base">Overview</h6>
-          <div className="grid grid-cols-1 sm:grid-cols-3">
-            <p>Balance</p>
-            <p className="col-span-2">0.626038472049 HONEY</p>
+    <div className="grow p-6 max-sm:p-0 max-sm:py-4">
+      <div className="grid grid-cols-2 gap-4">
+        <div className="flex flex-col gap-4">
+          <div className="px-8 py-10 space-y-7 bg-white rounded-xl shadow-xl text-black text-sm">
+            <h6 className="font-semibold text-xl">Overview</h6>
+            <div className="grid grid-cols-1 sm:grid-cols-3">
+              <p className="font-medium">Balance</p>
+              <p className="col-span-2">0.626038472049 HONEY</p>
+            </div>
+            <div className="grid grid-cols-1 sm:grid-cols-3">
+              <p className="font-medium">Pokt Value</p>
+              <p className="col-span-2">761,106.39 USD (@ $0.21/POKT)</p>
+            </div>
+            <div className="grid grid-cols-1 sm:grid-cols-3">
+              <p className="font-medium">Staked (Non-custodial)</p>
+              <p className="col-span-2">1,234,123.67 POKT</p>
+            </div>
           </div>
-          <div className="grid grid-cols-1 sm:grid-cols-3">
-            <p>Value</p>
-            <p className="col-span-2">844.76 ( @ 1,173.84 / HONEY)</p>
-          </div>
-          <div className="grid grid-cols-1 sm:grid-cols-3">
-            <p>Token</p>
-            <div className="dropdown col-span-2">
-              <div
-                className="flex justify-between w-1/2 border rounded border-gray-4 py-1 px-2"
-                tabIndex={0}
-              >
-                <p className=" ">$53,786.17</p>
-                <ChevronDownIcon className="w-5 h-5" />
+
+          <div className="px-8 py-10 space-y-7 bg-white rounded-xl shadow-xl text-black text-sm">
+            <h6 className="font-semibold text-xl">Non-custodial Info</h6>
+
+            <div className="grid grid-cols-1 sm:grid-cols-3">
+              <p className="font-medium">Total Nodes</p>
+              <div className="col-span-2">
+                <div className="bg-white grid grid-cols-4 font-medium rounded-full p-2 divide-x-2 text-center gap-4 rounded-ful border border-neutral-300 w-full">
+                  <div>15K</div>
+                  <div>30K</div>
+                  <div>45K</div>
+                  <div>60K</div>
+                </div>
+                <div className="grid grid-cols-4 pt-1 text-center gap-4 w-full">
+                  <div>0</div>
+                  <div>2</div>
+                  <div>0</div>
+                  <div>190</div>
+                </div>
               </div>
-              <ul
-                tabIndex={0}
-                className="dropdown-content menu p-2 shadow-xl bg-white rounded-box w-1/2"
-              >
-                <li>
-                  <a>Item 1</a>
-                </li>
-                <li>
-                  <a>Item 2</a>
-                </li>
-              </ul>
+            </div>
+
+            <div className="grid grid-cols-1 sm:grid-cols-3">
+              <p className="font-medium">Avg POKT Production</p>
+              <div className="col-span-2">
+                <div className="bg-white grid grid-cols-3 font-medium rounded-full p-2 divide-x-2 text-center gap-4 rounded-ful border border-neutral-300 w-full">
+                  <div>Daily</div>
+                  <div>Weekly</div>
+                  <div>Monthly</div>
+                </div>
+                <div className="grid grid-cols-3 font-semibold pt-1 text-center gap-4 w-full">
+                  <div>7.03 POKT</div>
+                  <div>49.21 POKT</div>
+                  <div>196.84 POKT</div>
+                </div>
+              </div>
+            </div>
+
+            <div className="grid grid-cols-1 sm:grid-cols-3">
+              <p className="font-medium text-xl">Node Info</p>
+            </div>
+
+            <div className="grid grid-cols-1 sm:grid-cols-3">
+              <p className="font-medium">Balance</p>
+              <p className="col-span-2">45,010.00 POKT</p>
+            </div>
+
+            <div className="grid grid-cols-1 sm:grid-cols-3">
+              <p className="font-medium">Jailed</p>
+              <p className="col-span-2">45,010.00 POKT</p>
+            </div>
+
+            <div className="grid grid-cols-1 sm:grid-cols-3">
+              <p className="font-medium"> Public Key</p>
+              <p className="col-span-2">45,010.00 POKT</p>
+            </div>
+
+            <div className="grid grid-cols-1 sm:grid-cols-3">
+              <p className="font-medium"> Service URL</p>
+              <p className="col-span-2">nodefleet.org</p>
+            </div>
+
+            <div className="grid grid-cols-1 sm:grid-cols-3">
+              <p className="font-medium"> Domain</p>
+              <p className="col-span-2">nodefleet.org</p>
             </div>
           </div>
         </div>
-        <div className="col-span-2 flex flex-col px-5 pt-3 pb-5 bg-white rounded-xl shadow-xl">
-          <div className="w-full max-w-[145px] h-auto mb-3">
-            <QRCode
-              size={256}
-              style={{ height: "auto", maxWidth: "100%", width: "100%" }}
-              value={address}
-              viewBox={`0 0 256 256`}
-            />
+
+        <div className="mt-8 md:mt-0 p-5 max-sm:mt-0 bg-white rounded-3xl shadow-lg w-full">
+          <div className="p-4 flex justify-between text-black">
+            <p className=" font-semibold text-xl">Performance</p>
+            <div className="relative w-28">
+              <label className="absolute right-2 top-3 translate-y-0.5 cursor-pointer">
+                <i className="fa-solid fa-angle-down"></i>
+              </label>
+              <select
+                className="border border-black py-3 text-base px-4 outline-none rounded-full appearance-none w-full cursor-pointer relative z-10 bg-transparent"
+                id="selectMo"
+              >
+                <option value={1}>Last 24H</option>
+                <option value={2}>Monthly</option>
+              </select>
+            </div>
           </div>
-          <p className="text-gray-4 text-base font-semibold">Address:</p>
-          <div className="flex items-center mt-2">
-            <p className=" text-gray-4 text-sm">{address}</p>
-            <DocumentDuplicateIcon className="w-4 h-4 ml-4 cursor-pointer" />
+          <div className="w-full h-full max-h-96">
+            <TransactionsChart data={weeksArray} />
           </div>
         </div>
       </div>
       <div className="mt-3 ">
-        <div className="flex text-gray-3 font-semibold">
+        <div className="flex text-black font-semibold">
           <a className="px-4 py-1 bg-white rounded-t-xl ">Transactions</a>
         </div>
         <div className="bg-white px-5 py-4 rounded-r-xl rounded-bl-xl shadow-lg overflow-x-auto">
