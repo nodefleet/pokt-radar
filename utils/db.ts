334872dfca8774ae105c131b170dc34445791f0d
XJuanCarlosXD <juancarlos192003@gmail.com>
2024-03-02 18:32:24 -0400

    update db.ts

--- utils/db.ts
+++ utils/db.ts
@@ -1,13 +1,13 @@
-// import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 
-// const globalForPrisma = global as unknown as {
-//   prisma: PrismaClient | undefined;
-// };
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
-// export const prisma =
-//   globalForPrisma.prisma ??
-//   new PrismaClient({
-//     log: ["query", "error", "info"],
-//   });
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ["query", "error", "info"],
+  });
 
-// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
