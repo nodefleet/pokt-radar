0b89be74eac7c76a86bbe321a4129f19ccc65ae1
XJuanCarlosXD <juancarlos192003@gmail.com>
2024-04-17 16:15:32 -0400

    update InputSearch.tsx

--- components/InputSearch.tsx
+++ components/InputSearch.tsx
@@ -1,4 +1,5 @@
 import axios from "axios";
+import Link from "next/link";
 import React, { useEffect, useState } from "react";
 
 interface InputSearchProps {
@@ -21,38 +22,66 @@ const InputSearch: React.FC<InputSearchProps> = ({
   search,
 }) => {
   const [value, setValue] = useState("");
+  const [dataSeach, setDataSearch] = useState<{
+    type: string;
+    data: any;
+  } | null>(null);
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const newValue = event.target.value;
+    setDataSearch(null);
     setValue(newValue);
     if (setCurrentValue) setCurrentValue(newValue);
   };
 
   useEffect(() => {
     const searhData = async () => {
+      const apiUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
       try {
-        const apiUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
         const blockResponse = await axios.get(
           `${apiUrl}/api/block?height=${Number(
             value.trim()
           )}&skip=${0}&take=${10}`
         );
+        if (blockResponse.data.block) {
+          return setDataSearch({
+            type: "block",
+            data: blockResponse.data.block,
+          });
+        }
+      } catch (error) {
+        console.error("Error al buscar datos:", error);
+      }
+      try {
         const addressResponse = await axios.get(
           `${apiUrl}/api/address?address=${value.trim()}&SKIP=${10}`
         );
+        if (addressResponse.data.account) {
+          return setDataSearch({
+            type: "account",
+            data: addressResponse.data.account,
+          });
+        }
+      } catch (error) {
+        console.error("Error al buscar datos:", error);
+      }
+      try {
         const transactionResponse = await axios.get(
           `${apiUrl}/api/transaction?hash=${value.trim()}`
         );
-
-        console.log("Datos de bloque:", blockResponse.data);
-        console.log("Datos de dirección:", addressResponse.data);
-        console.log("Datos de transacción:", transactionResponse.data);
+        if (transactionResponse.data.transation) {
+          return setDataSearch({
+            type: "transaction",
+            data: transactionResponse.data.transation,
+          });
+        }
       } catch (error) {
-        console.log(error);
         console.error("Error al buscar datos:", error);
       }
     };
     if (value.trim() !== "") {
       searhData();
+    } else {
+      setDataSearch(null);
     }
   }, [value]);
 
@@ -71,9 +100,20 @@ const InputSearch: React.FC<InputSearchProps> = ({
           className={`fa-solid fa-search ${classIconName} relative right-8 pt-4 group-hover:text-black_custom text-gray-400`}
         ></i>
       </div>
-      {search && (
-        <div className="bg-white rounded-lg p-4 mx-5 mr-10 border border-black z-20">
-          <h2 className="">Transaction</h2>
+      {search && value.trim() !== "" && (
+        <div className="bg-white rounded-full fixed p-2 px-10 mx-5 mr-10 border border-black z-20">
+          {dataSeach ? (
+            <Link
+              href={`/${
+                dataSeach?.type === "account" ? "address" : dataSeach?.type
+              }/${value.trim()}`}
+              className="capitalize font-medium text-xl hover:text-blue_primary"
+            >
+              {dataSeach?.type}
+            </Link>
+          ) : (
+            <i className="fas fa-spinner animate-spin text-2xl"></i>
+          )}
         </div>
       )}
     </div>
